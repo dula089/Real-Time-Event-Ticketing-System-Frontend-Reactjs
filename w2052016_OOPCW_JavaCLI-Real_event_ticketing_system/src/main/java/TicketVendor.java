e8b7b1d880689f0ab75f4ddffafc3367abed9e62
Dulani Kariyawasam <dulani.20231280@iit.ac.lk>
2024-12-04 19:49:10 +0530

    Update TicketVendor class

--- w2052016_OOPCW_JavaCLI-Real_event_ticketing_system/src/main/java/TicketVendor.java
+++ w2052016_OOPCW_JavaCLI-Real_event_ticketing_system/src/main/java/TicketVendor.java
@@ -1,2 +1,22 @@
-public class TicketVendor {
+class TicketVendor implements Runnable{
+    private final TicketPool ticketPool;
+    private final int ticketReleaseRate;
+
+    public TicketVendor(TicketPool ticketPool, int ticketReleaseRate){
+        this.ticketPool = ticketPool;
+        this.ticketReleaseRate = ticketReleaseRate;
+    }
+
+    @Override
+    public void run(){
+        try{
+            while(true){
+                ticketPool.addTickets(ticketReleaseRate);
+                Thread.sleep(1000);
+            }
+        }catch (InterruptedException e){
+            System.out.println("Vendor Interrupted.");
+            Thread.currentThread().interrupt();
+        }
+    }
 }
