46b908cee72577f02884b99a94e1d87d32c90a2f
Dulani Kariyawasam <dulani.20231280@iit.ac.lk>
2024-12-04 19:54:33 +0530

    Update Customer class

--- w2052016_OOPCW_JavaCLI-Real_event_ticketing_system/src/main/java/Customer.java
+++ w2052016_OOPCW_JavaCLI-Real_event_ticketing_system/src/main/java/Customer.java
@@ -1,2 +1,21 @@
-public class Customer {
+class Customer implements Runnable {
+    private final TicketPool ticketPool;
+    private final int ticketRetrievalRate;
+
+    public Customer(TicketPool ticketPool,int ticketRetrievalRate){
+        this.ticketPool= ticketPool;
+        this.ticketRetrievalRate = ticketRetrievalRate;
+    }
+    @Override
+    public  void run(){
+        try{
+            while(true){
+                ticketPool.removeTickets(ticketRetrievalRate);
+                Thread.sleep(1000);
+            }
+        }catch (InterruptedException e){
+            System.out.println("Customer interrupted.");
+            Thread.currentThread().interrupt();
+        }
+    }
 }
