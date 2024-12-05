2078dbff9c45baf6c4b534bdc52dbab3c85ca992
Dulani Kariyawasam <dulani.20231280@iit.ac.lk>
2024-12-05 20:11:15 +0530

    Update TicketPool class

--- w2052016_OOPCW_JavaCLI-Real_event_ticketing_system/src/main/java/TicketPool.java
+++ w2052016_OOPCW_JavaCLI-Real_event_ticketing_system/src/main/java/TicketPool.java
@@ -1,2 +1,31 @@
-public class TicketPool {
+import java.util.LinkedList;
+
+class TicketPool {
+    private final LinkedList<Integer>tickets=new LinkedList<>();
+    private final int capacity;
+
+    public TicketPool(int capacity){
+        this.capacity=capacity;
+    }
+
+    public synchronized void addTickets(int count) throws InterruptedException{
+        while(tickets.size()+ count> capacity){
+            wait();
+        }
+        for(int i=0; i<count; i++){
+            tickets.add(1);
+        }
+        System.out.println("Added "+count+" tickets. Total available: "+ tickets.size());
+        notifyAll();
+    }
+    public synchronized  void removeTickets(int count) throws InterruptedException{
+        while(tickets.size()<count){
+            wait();
+        }
+        for(int i=0; i<count; i++){
+            tickets.remove();
+        }
+        System.out.println("Removed "+count+" tickets. Remaining: "+tickets.size());
+        notifyAll();
+    }
 }
