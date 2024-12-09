8fc2e3cb89c69a0750e35ab0c22f2e759f2d4843
Dulani Kariyawasam <dulani.20231280@iit.ac.lk>
2024-12-09 19:49:42 +0530

    modify control panel

--- frontend/src/components/ControlPanel.jsx
+++ frontend/src/components/ControlPanel.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const ControlPanel = ({ onStart, onStop }) => {
   return (
-    <div className="flex justify-between space-x-96">
+    <div className="flex justify-between space-x-96 ">
       <button
         onClick={onStart}
-        className="bg-green-500 hover:bg-green-700 text-white font-bold py-16 px-14 rounded-full focus:outline-none focus:shadow-outline"
+        className="bg-green-500 hover:bg-green-700 text-white font-bold py-10 px-12 rounded-full focus:outline-none focus:shadow-outline "
       >
         Start
       </button>
       <button
         onClick={onStop}
-        className="bg-red-500 hover:bg-red-700 text-white font-bold py-16 px-14 rounded-full focus:outline-none focus:shadow-outline"
+        className="bg-red-500 hover:bg-red-700 text-white font-bold py-10 px-12 rounded-full focus:outline-none focus:shadow-outline"
       >
         Stop
       </button>
