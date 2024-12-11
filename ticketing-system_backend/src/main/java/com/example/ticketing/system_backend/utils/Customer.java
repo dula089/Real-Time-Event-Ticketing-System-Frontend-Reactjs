e4e1048ff442b02cee517000c4259a39733346dc
Dulani Kariyawasam <dulani.20231280@iit.ac.lk>
2024-12-11 19:38:28 +0530

    create customer class

---
+++ ticketing-system_backend/src/main/java/com/example/ticketing/system_backend/utils/Customer.java
@@ -0,0 +1,25 @@
+package com.example.ticketing.system_backend.utils;
+
+import com.example.ticketing.system_backend.model.TicketPool;
+
+public class Customer implements Runnable{
+    private final TicketPool ticketPool;
+    private final int retrievalRate;
+
+    public Customer(TicketPool ticketPool,int retrievalRate){
+        this.ticketPool=ticketPool;
+        this.retrievalRate=retrievalRate;
+    }
+    @Override
+    public void run(){
+        while (true){
+            if(!ticketPool.removeTicket()){
+                try{
+                    Thread.sleep(1000);
+                }catch (InterruptedException e){
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+}
