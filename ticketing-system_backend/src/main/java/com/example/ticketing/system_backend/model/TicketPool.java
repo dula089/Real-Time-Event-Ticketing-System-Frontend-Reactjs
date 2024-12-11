2252a1fc94b2881c9bec169bc243069f51c3be58
Dulani Kariyawasam <dulani.20231280@iit.ac.lk>
2024-12-11 23:11:34 +0530

    create and update

--- ticketing-system_backend/src/main/java/com/example/ticketing/system_backend/model/TicketPool.java
+++ ticketing-system_backend/src/main/java/com/example/ticketing/system_backend/model/TicketPool.java
@@ -3,30 +3,36 @@ package com.example.ticketing.system_backend.model;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class TicketPool {
-    private AtomicInteger ticketsAvailable;
-    private final int maxCapacity;
+    private AtomicInteger ticketsAvailable;//Thread safe counter for tickets
+    private final int maxCapacity;//maximum allowed tickets in the pool
 
-    public TicketPool(int totalTickets, int maxCapacity){
-        this.ticketsAvailable=new AtomicInteger(totalTickets);
-        this.maxCapacity=maxCapacity;
+    //constructor to initialize total tickets and max capacity
+    public TicketPool(int totalTickets, int maxCapacity) {
+        this.ticketsAvailable = new AtomicInteger(totalTickets);
+        this.maxCapacity = maxCapacity;
     }
 
-    public synchronized void addTickets(int number){
-        if (ticketsAvailable.get()+number<=maxCapacity){
+    //synchronized method to add tickets to the pool
+    public synchronized void addTickets(int number) {
+        if (ticketsAvailable.get() + number <= maxCapacity) {
             ticketsAvailable.addAndGet(number);
-        }else{
+        } else {
             throw new IllegalStateException("Cannot add more tickets, max capacity reached.");
         }
     }
-    public synchronized boolean removeTicket(){
-        if(ticketsAvailable.get()>0){
+
+    //synchronized method to remove a ticket from the pool
+    public synchronized boolean removeTicket() {
+        if (ticketsAvailable.get() > 0) {
             ticketsAvailable.decrementAndGet();
             return true;
-        }else {
+        } else {
             return false;
         }
     }
-    public int getTicketsAvailable(){
+
+    //method to get the current number of available tickets
+    public int getTicketsAvailable() {
         return ticketsAvailable.get();
     }
 }
--- ticketing-system_backend/src/main/java/com/example/ticketing/system_backend/utils/Vendor.java
+++ ticketing-system_backend/src/main/java/com/example/ticketing/system_backend/utils/Vendor.java
@@ -6,18 +6,20 @@ public class Vendor implements Runnable {
     private final TicketPool ticketPool;
     private final int releaseRate;
 
-    public Vendor(TicketPool ticketPool,int releaseRate){
-        this.ticketPool=ticketPool;
-        this.releaseRate=releaseRate;
+    //constructor to initialize the ticket pool and release rate
+    public Vendor(TicketPool ticketPool, int releaseRate) {
+        this.ticketPool = ticketPool;
+        this.releaseRate = releaseRate;
     }
+
     @Override
-    public void run(){
-        while (true){
-            ticketPool.addTickets(releaseRate);
-            try{
-                Thread.sleep(1000);
-            }catch (InterruptedException e){
-                Thread.currentThread().interrupt();
+    public void run() {
+        while (true) {
+            ticketPool.addTickets(releaseRate);//add tickets to the pool
+            try {
+                Thread.sleep(1000);//wait for 1 second before releasing more tickets
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();//restore interrupted status
             }
         }
     }
