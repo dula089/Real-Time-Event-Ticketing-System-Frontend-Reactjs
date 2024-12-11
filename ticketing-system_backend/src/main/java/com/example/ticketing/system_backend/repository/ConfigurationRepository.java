03fbff0a4fcc4b57cc871a0220a8c08191e56039
Dulani Kariyawasam <dulani.20231280@iit.ac.lk>
2024-12-11 23:08:59 +0530

    modify configurationrepository class

--- ticketing-system_backend/src/main/java/com/example/ticketing/system_backend/repository/ConfigurationRepository.java
+++ ticketing-system_backend/src/main/java/com/example/ticketing/system_backend/repository/ConfigurationRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ConfigurationRepository extends JpaRepository<Configuration,Long> {
+public interface ConfigurationRepository extends JpaRepository<Configuration, Long> {
 }
